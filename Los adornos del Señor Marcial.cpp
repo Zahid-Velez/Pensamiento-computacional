9f099056862a1c390742bc48766791447b66d0a2
Zahid-Velez <gallegosvelezz@gmail.com>
2025-09-08 10:33:08 -0500

    Add files via upload

---
+++ Los adornos del Señor Marcial.cpp
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    double x, y, a;
+    cin>>x;
+    cin>>y;
+    a=((x*x)+(y*y));
+    cout<<sqrt(a);
+
+    return 0;
+}
