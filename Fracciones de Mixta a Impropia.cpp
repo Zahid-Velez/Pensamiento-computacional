9bf5b73c20a86b3dd7164e80dcddac09b76b5f24
Zahid-Velez <gallegosvelezz@gmail.com>
2025-09-08 11:06:46 -0500

    Add files via upload

---
+++ Fracciones de Mixta a Impropia.cpp
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    double x, y, z, a;
+    cin>>x;
+    cin>>y;
+    cin>>z;
+    a=(x*z+y);
+    cout<<a<<"/"<<z;
+    
+    return 0;
+}
