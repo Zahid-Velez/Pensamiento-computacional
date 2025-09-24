c27aadf76fb0485e35c94d87f403acedb98f3420
Zahid-Velez <gallegosvelezz@gmail.com>
2025-09-24 09:19:11 -0500

    Add files via upload

---
+++ Día de la semana.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int día;
+    cin>>día;
+    switch (día) {
+    case 1:
+        cout<<"Lunes";
+        break;==
+    case 2:
+        cout<<"Martes";
+        break; 
+    case 3:
+        cout<<"Miércoles";
+        break;
+    case 4:
+        cout<<"Jueves";
+        break;
+    case 5:
+        cout<<"Viernes";
+        break; 
+    case 6:
+        cout<<"Sábado";
+        break;   
+    case 7:
+        cout<<"Domingo";
+        break;
+    default:
+        cout<<"Error";                  
+        break;
+    }
+    return 0;  
+}      
